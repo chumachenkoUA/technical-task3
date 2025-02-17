53aa37bc929f9bd29c33bf606e1d793838ee3f7f
cumachenko <kirito22122005@gmail.com>
2025-02-18 00:39:28 +0200

    made a dynamic list for gallery

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,16 +1,30 @@
+interface galleryItem{
+    src:string;
+    alt:string;
+    brand:string;
+    founders:string;
+}
 
-function Gallery() {
+interface galleryProps{
+    galleryItems:galleryItem[];
+}
+
+
+function Gallery({galleryItems}:galleryProps) {
 
     return (
         <>
             <div className={"gallery"}>
                 <span className={"gallery__title"}></span>
                 <ul className={"gallery__list"}>
-                    <li className={"gallery__item"}>
-                        <img className={"gallery__image"} src={""} alt=""/>
-                        <h2 className={"gallery__brand"}></h2>
-                        <span className={"gallery__founders"}></span>
-                    </li>
+                    {galleryItems.map((item,index)=>(
+                        <li className={"gallery__item"} key={index}>
+                            <img className={"gallery__image"} src={item.src} alt={item.alt}/>
+                            <h2 className={"gallery__brand"}>{item.brand}</h2>
+                            <p className={"gallery__founders"}>{item.founders}</p>
+                        </li>
+                    ))}
+
                 </ul>
             </div>
         </>
