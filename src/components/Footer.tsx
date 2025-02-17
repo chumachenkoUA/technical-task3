943bf2ad59042b954e1bced3dc7dcc37c4509b09
cumachenko <kirito22122005@gmail.com>
2025-02-17 21:27:27 +0200

    add Props

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,12 @@
+interface footerItem {
+    title: string;
+    link: string;
+}
+interface FooterProps {
+    brands:footerItem[];
+    company: footerItem[];
+    socials: footerItem[];
+}
 
 function Footer() {
 
