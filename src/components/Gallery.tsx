14d562b542b0f90bf77ddfbb53f373a2e2a6dfed
cumachenko <kirito22122005@gmail.com>
2025-02-17 18:52:08 +0200

    clear

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -3,16 +3,7 @@ function Gallery() {
 
     return (
         <>
-            <div className={"gallery"}>
-                <span className={"gallery__title"}></span>
-                <ul className={"gallery__list"}>
-                    <li className={"gallery__item"}>
-                        <img className={"gallery__image"} src={""} alt=""/>
-                        <h2 className={"gallery__brand"}></h2>
-                        <span className={"gallery__founders"}></span>
-                    </li>
-                </ul>
-            </div>
+           
         </>
     )
 }
